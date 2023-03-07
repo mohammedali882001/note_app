9bb6f05914148a5fab0ee3e238d20ccd3b2f08fa
mohammed ali <leomohammed882001@gmail.com>
2023-03-07 04:09:10 +0200

    ceate notes app

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(),
+    );
   }
 }

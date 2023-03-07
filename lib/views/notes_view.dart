cfeee172e044052c399de6e95d37e94cce0b5f7d
mohammed ali <leomohammed882001@gmail.com>
2023-03-07 07:06:20 +0200

    create notes list view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../widgets/custom_notes_item.dart';
+import '../widgets/custom_notes_listview.dart';
 import '../widgets/custom_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -16,65 +18,9 @@ class NotesView extends StatelessWidget {
           children: const [
             SizedBox(height: 54),
             CustomViewBody(),
-            CustomNotesItem(),
+            Expanded(child: CustomNotesListView()),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class CustomNotesItem extends StatelessWidget {
-  const CustomNotesItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 24, top: 24, left: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: const Color(0xffffcc80),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 32,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Text(
-                  'Build Your Career With Mohammed Ali ',
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.5),
-                    fontSize: 22,
-                  ),
-                ),
-              ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.delete_rounded,
-                  color: Colors.black,
-                  size: 32,
-                ),
-              )),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May 22 , 2022',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.5),
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
---
+++ lib/widgets/custom_notes_listview.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'custom_notes_item.dart';
+
+class CustomNotesListView extends StatelessWidget {
+  const CustomNotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8),
+            child: CustomNotesItem(),
+          );
+        },
+      ),
+    );
+  }
+}

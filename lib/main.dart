ddc79186f7f886e0023bb8af92555a34b0d0c647
mohammed ali <leomohammed882001@gmail.com>
2023-03-09 23:38:31 +0200

    adding addNote cubit , registerAdapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/notes_view.dart';
 
 import 'constants.dart';
@@ -7,6 +8,7 @@ import 'constants.dart';
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNoteBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
